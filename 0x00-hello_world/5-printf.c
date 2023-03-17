b42d661832af99c0f80a97cdc4619836c2fa79c2
Mrscan20 <khalid.learning18@gmail.com>
2023-03-16 18:33:13 -0700

    a C program that prints exactly -2-

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#inlcude <stdio.h>
+/* 
+ * Author : khalid.lakdar
+ */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
