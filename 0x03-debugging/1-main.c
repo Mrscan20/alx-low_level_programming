612f06cf1c7c7229d76ad456202a74b4c0f79f42
Mrscan20 <khalid.learning18@gmail.com>
2023-03-23 15:43:40 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-        int i;
+int i;
 
-        printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
-        i = 0;
+i = 0;
 
-        while (i > 10)
-        {
-                putchar(i); /* this part of code causing the infinte loop for not increment i */
-        }
+while (i > 10)
+{
+putchar(i); /* part causing the infinte loop for not increment i */
+}
 
-        printf("Infinite loop avoided! \\o/\n");
+printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
+return (0);
 }
