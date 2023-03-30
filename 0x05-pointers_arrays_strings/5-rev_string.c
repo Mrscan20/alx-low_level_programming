06cb6173dec5f7692ca1ab3b117a162b87a9d512
Mrscan20 <kal@meldiodas.localdomain>
2023-03-30 00:40:14 +0000

    function that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *rev_string - function that reverse a string
+ *@s: pointeur s as a parameter.
+ *
+ *Return:void.
+ */
+void rev_string(char *s)
+{
+int i;
+char tmp[10];
+int j = 0;
+
+for (i = 8; i >= 0; i--)
+	{
+	*(tmp + j) = *(s + i);
+	j++;
+	}
+tmp[9] = '\0';
+for (j = 0; j < 10; j++)
+	*(s + j) = tmp[j];
+}
+
