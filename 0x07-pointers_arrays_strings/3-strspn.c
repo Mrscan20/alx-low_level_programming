11a1f8f8ee259657148f0c0214e8fe773f71db55
Mrscan20 <khalid.learning18@gmail.com>
2023-04-04 03:14:46 +0000

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,10 +12,10 @@ unsigned int _strspn(char *s, char *accept)
 unsigned int i, j;
 int n = 0;
 for (i = 0; *(s + i) != '\0'; i++)
-	{
-	for (j = 0; j < i; j++)	
-		if (*(accept + j) == *(s + i))
-		n++;
-	}
+{
+for (j = 0; j < i; j++)
+	if (*(accept + j) == *(s + i))
+	n++;
+}
 return (n);
 }
