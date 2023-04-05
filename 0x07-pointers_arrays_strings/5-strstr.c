d99ab9259dd744a4bb8e4e8c23d4d64f3d9b5dad
Mrscan20 <khalid.learning18@gmail.com>
2023-04-05 02:15:51 +0000

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,18 +11,17 @@ char *_strstr(char *haystack, char *needle)
 {
 int i;
 int len = 0;
+
 while (*(needle + len) != '\0')
-	len++;	
+	len++;
 for (i = 0; *(haystack + i) != '\0'; i++)
 {
 	int n1 = i;
-/*	for (j = 0; j <= len && *(needle + j) == *(haystack + i) ; j++)
-	{*/
+
 	if (*(needle + 0) == *(haystack + n1))
 	{
 		return (haystack + i);
 	}
 }
-	/*if (n1 == len) */
 return ('\0');
 }
