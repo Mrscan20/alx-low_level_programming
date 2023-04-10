a11d861098f451773f9fdc77a6289c426a4f289c
Mrscan20 <khalid.learning18@gmail.com>
2023-04-10 01:24:21 +0000

    print the program is name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point.
  * @argc: number of argument passed in the command line.
- * @argv; pointer to array contain argument passed as string.
+ * @argv: pointer to array contain argument passed as string.
  * Return: Always 0.
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 argc = 0;
 printf("%s\n", argv[argc]);
