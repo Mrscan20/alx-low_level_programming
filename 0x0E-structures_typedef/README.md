80686052afcb66701e01240789a9f337cfb7c9f6
Mrscan20 <khalid.learning18@gmail.com>
2023-04-18 02:17:10 +0000

    Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+C - Structures, typedef What are structures, when, why and how to use them
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - poppy
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Description: define type dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
