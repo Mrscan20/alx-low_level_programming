7cd77c18859bddcf02af5238b20523b7bf626ad9
Mrscan20 <khalid.learning18@gmail.com>
2023-04-21 02:16:49 +0000

    sum them all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -75,4 +75,4 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 int sum_them_all(const unsigned int n, ...);
-#endif 
+#endif
